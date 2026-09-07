96ceb806df47f38fbefe355e4e6b2ab914c91150
bathyz <148730912+bathyz@users.noreply.github.com>
2026-09-07 11:07:49 -0400

    Add variables for VPC and subnet configurations

---
+++ modules/network/variables.tf
@@ -0,0 +1,24 @@
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
